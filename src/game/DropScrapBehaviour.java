f56a76d94346392b09d6fbcf163305d3e144ef69
itsFifiii <160809101+itsFifiii@users.noreply.github.com>
2024-05-05 13:03:15 +1000

    added DropScrapBehaviour and StealScrapsBehaviour but it is not implemented

---
+++ src/game/DropScrapBehaviour.java
@@ -0,0 +1,14 @@
+package game;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.Behaviour;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class DropScrapBehaviour implements Behaviour {
+
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        return null;
+    }
+}
