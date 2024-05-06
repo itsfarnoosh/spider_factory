65e8ae94676dbbb2456c93d6c17914bf2d8e92be
hmaily <mailychoang@gmail.com>
2024-05-06 17:02:09 +1000

    Implement drink functionality of Puddle class

--- src/game/Puddle.java
+++ src/game/Puddle.java
@@ -1,9 +1,20 @@
 package game;
 
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 
 public class Puddle extends Ground {
     public Puddle() {
         super('~');
+    }
+
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction){
+        if (location.containsAnActor() && (location.getActor()).equals(actor)) {
+            return new ActionList(new DrinkAction());
+        }
+        return new ActionList();
     }
 }
