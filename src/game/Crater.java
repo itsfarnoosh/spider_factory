5b80287404f228f0883127d34b5b435aa2686550
Da Wei Gao <davidgao2013@gmail.com>
2024-05-07 21:38:53 +1000

    Refactored Crater class to be able to spawn a particular type of creature.

--- src/game/Crater.java
+++ src/game/Crater.java
@@ -11,14 +11,17 @@ import java.util.Random;
  * Class representing Crater Ground type.
  */
 public class Crater extends Ground {
-    private Enemy spawnCreature;
+
+    private int indexToSpawn;
 
     /**
      * Constructor for crater, a ground object than can spawn huntsman spiders.
      */
-    public Crater(Enemy spawnCreature) {
+    public Crater() {
         super('u');
-        this.spawnCreature = spawnCreature;
+        int numOfCreatures = 3;
+        Random random = new Random();
+        this.indexToSpawn = random.nextInt(numOfCreatures);
     }
 
 
@@ -29,15 +32,19 @@ public class Crater extends Ground {
      */
     @Override
     public void tick(Location location) {
+        ArrayList<Enemy> spawnCreatures = new ArrayList<Enemy>();
+        spawnCreatures.add(new HuntsmanSpider());
+        spawnCreatures.add(new AlienBug());
+        spawnCreatures.add((new SuspiciousAstronaut()));
         super.tick(location);
-        if (Math.random() <= this.spawnCreature.getSpawnChance()) {
+        if (Math.random() <= spawnCreatures.get(indexToSpawn).getSpawnChance()) {
             Random rand = new Random();
             while (true) {
                 Location spawnLocation = location.getExits().get(rand.nextInt(location.getExits().size()))
                         .getDestination();
-                if (spawnLocation.canActorEnter(this.spawnCreature)) {
+                if (spawnLocation.canActorEnter(spawnCreatures.get(indexToSpawn))) {
                     if (!spawnLocation.containsAnActor()) {
-                        spawnLocation.addActor(this.spawnCreature);
+                        spawnLocation.addActor(spawnCreatures.get(indexToSpawn));
                         break;
                     }
                 }
