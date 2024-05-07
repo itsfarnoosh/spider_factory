9182356edda695b46d2d494cdeae1d1fb781885d
Da Wei Gao <davidgao2013@gmail.com>
2024-05-07 20:38:57 +1000

    Refactored FollowBehaviour so it does not need a parameter in its constructor.

--- src/game/FollowBehaviour.java
+++ src/game/FollowBehaviour.java
@@ -1,6 +1,7 @@
 package game;
 
 
+import edu.monash.fit2099.demo.mars.Application;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.positions.Exit;
@@ -8,11 +9,12 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.actions.MoveActorAction;
 import edu.monash.fit2099.engine.actors.Behaviour;
+import java.util.Objects;
 
 /**
  * A class that figures out a MoveAction that will move the actor one step
  * closer to a target Actor.
- * @see edu.monash.fit2099.demo.mars.Application
+ * @see Application
  *
  * Created by:
  * @author Riordan D. Alfredo
@@ -21,26 +23,31 @@ import edu.monash.fit2099.engine.actors.Behaviour;
  */
 public class FollowBehaviour implements Behaviour {
 
-    private final Actor target;
-
-    /**
-     * Constructor.
-     *
-     * @param subject the Actor to follow
-     */
-    public FollowBehaviour(Actor subject) {
-        this.target = subject;
-    }
+    private Actor target = null;
 
     @Override
     public Action getAction(Actor actor, GameMap map) {
-        if(!map.contains(target) || !map.contains(actor))
-            return null;
 
         Location here = map.locationOf(actor);
-        Location there = map.locationOf(target);
+        Location there = null;
+        int currentDistance = 999999999;
+        if (Objects.isNull(this.target)) {
+            for (Exit exit : here.getExits()) {
+                Location destination = exit.getDestination();
+                if (destination.containsAnActor()) {
+                    if (destination.getActor() instanceof Player) {
+                        this.target = destination.getActor();
+                        there = map.locationOf(target);
+                        currentDistance = distance(here, there);
+                    }
+                }
+            }
+        }
 
-        int currentDistance = distance(here, there);
+        if (Objects.isNull(this.target)) {
+            return null;
+        }
+
         for (Exit exit : here.getExits()) {
             Location destination = exit.getDestination();
             if (destination.canActorEnter(actor)) {
