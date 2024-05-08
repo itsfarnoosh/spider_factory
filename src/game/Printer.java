f70a44a54a5bb040f601d69836b50b337e13d1d8
zhen00101 <zhen0010@student.monash.edu>
2024-05-08 20:56:29 +1000

    Printer interface

---
+++ src/game/Printer.java
@@ -0,0 +1,27 @@
+package game;
+
+import edu.monash.fit2099.engine.items.Item;
+
+public interface Printer {
+
+    /**
+     * Weather or not the printer prints the item
+     *
+     * @return true if item is printed, else false
+     */
+    boolean printsItem();
+
+    /**
+     * Getter to get the item
+     *
+     * @return the item
+     */
+    Item getItem();
+
+    /**
+     * The credit of the item
+     *
+     * @return the credit
+     */
+    int creditPayable();
+}
