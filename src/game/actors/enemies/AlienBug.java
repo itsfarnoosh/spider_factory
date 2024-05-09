ddd9281a17a26c1c5e9175889031b8b2e8c5f016
hmaily <mailychoang@gmail.com>
2024-05-10 00:30:42 +1000

    Implement drop behaviour of AlienBug when unconscious - Overwrote the unconscious method to allow for AlienBug to drop all inventory - Correctly implement req2 - Correctly implement display strings to allow user to see what items are being dropped

--- src/game/actors/enemies/AlienBug.java
+++ src/game/actors/enemies/AlienBug.java
@@ -31,7 +31,9 @@ import java.util.*;
  * AlienBug class that extends Enemy and includes behaviors for following and collecting scraps.
  */
 public class AlienBug extends Enemy {
-    /** Map of priorities to Behaviours */
+    /**
+     * Map of priorities to Behaviours
+     */
     private Map<Integer, Behaviour> behaviours = new TreeMap<>();
     private List<Item> potentialItems = new ArrayList<>();
 
@@ -40,7 +42,6 @@ public class AlienBug extends Enemy {
 
     /**
      * Constructor.
-     *
      */
     public AlienBug() {
         super(generateUniqueName(), 'a', 2, Monster.ALIEN_BUG);
@@ -50,10 +51,12 @@ public class AlienBug extends Enemy {
         this.addCapability(Ability.ENTER_SPACESHIP);
         this.addCapability(Status.ALIEN_BUG);
         this.spawnChance = 0.1;
+
     }
 
     /**
      * Generates a unique name for each Alien Bug instance.
+     *
      * @return A string representing the unique name "Feature-XXX" where XXX are random digits
      */
     private static String generateUniqueName() {
@@ -61,12 +64,14 @@ public class AlienBug extends Enemy {
         return "Feature-" + randomDigits;
     }
     //the Alien Bug cannot attack the Intern.
+
     /**
      * Called once per turn, allows the Alien Bug to perform actions like moving or stealing.
-     * @param actions The list of possible actions this turn
+     *
+     * @param actions    The list of possible actions this turn
      * @param lastAction The last action performed by this Actor
-     * @param map The game map containing this Actor
-     * @param display The I/O object to which messages may be written
+     * @param map        The game map containing this Actor
+     * @param display    The I/O object to which messages may be written
      * @return the Action this Actor chooses to perform this turn
      */
     @Override
@@ -78,12 +83,7 @@ public class AlienBug extends Enemy {
             if (action != null) {
                 return action;
             }
-            if(!this.isConscious()){
-                // Drop all items if the AlienBug is not conscious (dead)
-                List<Item> itemsToDrop = new ArrayList<>(this.getItemInventory());
-                for (Item item : itemsToDrop) {
-                    actions.add(new DropAction(item));
-                }}
+
         }
         return new DoNothingAction();
     }
@@ -100,10 +100,33 @@ public class AlienBug extends Enemy {
     @Override
     public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
         ActionList actions = new ActionList();
-        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
+        if (otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
             actions.add(new AttackAction(this, direction));
         }
         return actions;
     }
 
+    public List<String> dropAllScraps(GameMap map) {
+        List<Item> itemsToDrop = new ArrayList<>(this.getItemInventory());
+        List<String> actionDescriptions = new ArrayList<>();
+        for (Item item : itemsToDrop) {
+            Action dropAction = new DropAction(item);
+            String result = dropAction.execute(this, map);
+            actionDescriptions.add(result);
+        }
+        return actionDescriptions;
+    }
+
+    @Override
+    public String unconscious(Actor actor, GameMap map){
+        List<String> dropDescriptions = dropAllScraps(map);
+        map.removeActor(this);
+        return this + " met their demise.\n" + String.join("\n", dropDescriptions);
+    }
+
+    public String unconscious(GameMap map) {
+        List<String> dropDescriptions = dropAllScraps(map);
+        map.removeActor(this);
+        return this + " ceased to exist\n." + String.join("\n", dropDescriptions);
+    }
 }
